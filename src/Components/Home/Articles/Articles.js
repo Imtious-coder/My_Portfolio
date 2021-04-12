2517e58ee8ee6feedf9c85f7657c2afe6a834cbd
unknown <iarafat386@gmail.com>
2021-04-12 20:29:07 +0600

    UI updated

--- src/Components/Home/Articles/Articles.js
+++ src/Components/Home/Articles/Articles.js
@@ -2,6 +2,8 @@ import React from 'react';
 import coffee from '../../../Images/coffee.jfif';
 import './Articles.css';
 import '../../../../node_modules/hover.css/css/hover-min.css';
+import E from '../../../Images/earth vs mars.jfif';
+import Mask from '../../../Images/aMask.jfif';
 
 const Articles = () => {
     return (
@@ -21,31 +23,31 @@ const Articles = () => {
                         <div class="card Articles">
                             <img src={coffee} class="card-img-top" alt="..." />
                             <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                                <h5 class="card-title">C for C O F F E E</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up.</p>
+                                <a href="#" class="btn btn-primary">More</a>
                             </div>
                         </div>
                     </div>
 
                     <div className="col-md-4 hvr-wobble-top">
                         <div class="card Articles">
-                            <img src={coffee} class="card-img-top" alt="..." />
+                            <img src={E} class="card-img-top" alt="..." />
                             <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                                <h5 class="card-title">Why M A R S</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up.</p>
+                                <a href="#" class="btn btn-primary">More</a>
                             </div>
                         </div>
                     </div>
 
                     <div className="col-md-4">
                         <div class="card Articles hvr-wobble-bottom">
-                            <img src={coffee} class="card-img-top " alt="..." />
+                            <img src={Mask} class="card-img-top " alt="..." />
                             <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
+                                <h5 class="card-title">M A S K</h5>
                                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                                <a href="#" class="btn btn-primary">More</a>
                             </div>
                         </div>
                     </div>
--- src/Components/Home/Projects/Projects.js
+++ src/Components/Home/Projects/Projects.js
@@ -2,6 +2,12 @@ import React from 'react';
 import AM1 from '../../../Images/A_Music.png';
 import AM2 from '../../../Images/A_Music_Search.png';
 import AM3 from '../../../Images/A_Music_Lyric.png';
+import Doctor1 from '../../../Images/Doctors1.png';
+import Doctor2 from '../../../Images/Doctors2.png';
+import Doctor3 from '../../../Images/Doctors3.png';
+import T1 from '../../../Images/T1.png';
+import T2 from '../../../Images/T2.png';
+import T3 from '../../../Images/T3.png';
 import './Projects.css';
 
 
@@ -12,7 +18,7 @@ const Projects = () => {
             <div className="row pb-5">
                 <div className="col-md-1"></div>
                 <div className="col-md-5 hvr-shadow-radial hvr-grow ImageBG">
-                    <div style={{ width: '100%', height: '100%', paddingTop:'10px', paddingBottom:'10px' }} id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+                    <div style={{ width: '100%', height: '100%', paddingTop: '10px', paddingBottom: '10px' }} id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
                         <div style={{ width: '100%', height: '100%', borderRadius: '5px' }} class="carousel-inner">
                             <div class="carousel-item active" data-bs-interval="2000">
                                 <img src={AM1} class="d-block w-100" alt="..." />
@@ -28,36 +34,38 @@ const Projects = () => {
                 </div>
                 <div className="col-md-1"></div>
                 <div className="col-md-4 hvr-hang Details">
-                    <h1 className="text-success">A_Music</h1>
-                    <h6 className="text-info">HTML5, CSS3, JavaScript, Bootstrap5</h6>
+                    <h1 className="text-primary">A_Music</h1>
+                    <h6 className="text-success">HTML5, CSS3, JavaScript, Bootstrap5</h6>
                     <small className="text-secondary">This is a lyric finding website. Made in Vanila JS. You can find any update music lyriq here.
                     Just put the name and hit the search button and you will get more then 7+ lyriq folder.
-                    </small>
+                    </small> <br />
+                    <a className="text-secondary" href="https://imtious-coder.github.io/A_Music/">Site</a>
                 </div>
             </div>
-
+            {/* Project 2 Doctors Portal */}
             <div className="row mt-5 pb-5 pt-5">
                 <div className="col-md-1"></div>
                 <div className="col-md-4 hvr-hang Details">
-                    <h1 className="text-success">A_Music</h1>
-                    <h6 className="text-info">HTML5, CSS3, JavaScript, Bootstrap5</h6>
-                    <small className="text-secondary">This is a lyric finding website. Made in Vanila JS. You can find any update music lyriq here.
-                    Just put the name and hit the search button and you will get more then 7+ lyriq folder.
-                    </small>
+                    <h1 className="text-primary">A_Doctors</h1>
+                    <h6 className="text-success">HTML5, CSS3, JavaScript, Bootstrap5, React, Express, Node.js, MaterialUI</h6>
+                    <small className="text-secondary">This is a lyric finding website. Made by following MERN system. You can get an apointmen from the doctors.
+                    This is my own project developed for practicing.
+                    </small> <br />
+                    <a className="text-secondary" href="https://keen-sinoussi-d5c500.netlify.app/">Site</a>
                 </div>
 
                 <div className="col-md-1"></div>
                 <div className="col-md-5 hvr-shadow-radial hvr-grow ImageBG">
-                    <div style={{ width: '100%', height: '100%', paddingTop:'10px', paddingBottom:'10px'  }} id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+                    <div style={{ width: '100%', height: '100%', paddingTop: '10px', paddingBottom: '10px' }} id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
                         <div style={{ width: '100%', height: '100%', borderRadius: '5px' }} class="carousel-inner">
                             <div class="carousel-item active" data-bs-interval="2000">
-                                <img src={AM1} class="d-block w-100" alt="..." />
+                                <img src={Doctor1} class="d-block w-100" alt="..." />
                             </div>
                             <div class="carousel-item" data-bs-interval="2000">
-                                <img src={AM2} class="d-block w-100" alt="..." />
+                                <img src={Doctor2} class="d-block w-100" alt="..." />
                             </div>
                             <div class="carousel-item" data-bs-interval="2000">
-                                <img src={AM3} class="d-block w-100" alt="..." />
+                                <img src={Doctor3} class="d-block w-100" alt="..." />
                             </div>
                         </div>
                     </div>
@@ -67,27 +75,27 @@ const Projects = () => {
             <div className="row mt-5 pt-5 pb-5">
                 <div className="col-md-1"></div>
                 <div className="col-md-5 hvr-shadow-radial hvr-grow ImageBG">
-                    <div style={{ width: '100%', height: '100%', paddingTop:'10px', paddingBottom:'10px'  }} id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+                    <div style={{ width: '100%', height: '100%', paddingTop: '10px', paddingBottom: '10px' }} id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
                         <div style={{ width: '100%', height: '100%', borderRadius: '5px' }} class="carousel-inner">
                             <div class="carousel-item active" data-bs-interval="2000">
-                                <img src={AM1} class="d-block w-100" alt="..." />
+                                <img src={T1} class="d-block w-100" alt="..." />
                             </div>
                             <div class="carousel-item" data-bs-interval="2000">
-                                <img src={AM2} class="d-block w-100" alt="..." />
+                                <img src={T2} class="d-block w-100" alt="..." />
                             </div>
                             <div class="carousel-item" data-bs-interval="2000">
-                                <img src={AM3} class="d-block w-100" alt="..." />
+                                <img src={T3} class="d-block w-100" alt="..." />
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="col-md-1"></div>
                 <div className="col-md-4 hvr-hang Details">
-                    <h1 className="text-success">A_Music</h1>
-                    <h6 className="text-info">HTML5, CSS3, JavaScript, Bootstrap5</h6>
-                    <small className="text-secondary">This is a lyric finding website. Made in Vanila JS. You can find any update music lyriq here.
-                    Just put the name and hit the search button and you will get more then 7+ lyriq folder.
-                    </small>
+                    <h1 className="text-primary">Temperature Finder</h1>
+                    <h6 className="text-success">HTML5, CSS3, JavaScript, Bootstrap5</h6>
+                    <small className="text-secondary">This is a temperature finding website. Made in Vanila JS. You can find any countries update temperature here.
+                    </small> <br />
+                    <a className="text-secondary" href="https://imtious-coder.github.io/Temperature-Finder/?">Site</a>
                 </div>
             </div>
             <div className="container">
