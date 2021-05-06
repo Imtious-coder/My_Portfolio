9174fc33eb84aa384971d6cb391ee341a78af466
unknown <iarafat386@gmail.com>
2021-05-06 13:15:32 +0600

    Projects Updated 1

--- src/Components/Home/Projects/Projects.js
+++ src/Components/Home/Projects/Projects.js
@@ -7,6 +7,8 @@ import './Projects.css';
 import { Link } from 'react-router-dom';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye, faCaretRight } from '@fortawesome/free-solid-svg-icons'
 
 const Projects = () => {
     useEffect(() => {
@@ -40,100 +42,188 @@ const Projects = () => {
             document.getElementById("Project1_Details4").style.display = "block";
         })
     }
+
+
+    // {/* <div className="row pb-5">
+    //                 {/* A_Doctors Card */}
+    //                 <div data-aos="fade-right" id="Project1" className="col-md-6 hvr-grow">
+    //                     <img src={Doctor1} class="d-block w-100 Images" alt="..." />
+    //                     {/* Details Button */}
+    //                     <button onMouseEnter={handleProject1} className="Details">Details</button>
+    //                 </div>
+
+    //                 <div id="Project1_Details1" className="col-md-6 justify-content-center ms-auto text-white">
+    //                     <div className="mb-5 ms-5">
+    //                         <h1>A_Doctors</h1>
+    //                         <p>Full Stack Project</p>
+    //                         <ul className="text-secondary">
+    //                             <li>Dentist Appointment taking website.</li>
+    //                             <li>Choose the service you want.</li>
+    //                             <li>Pick any flexible date for appointment.</li>
+    //                         </ul>
+    //                         <p className="text-secondary"> <b>Tech and Tools: </b>JavaScript, React, HTML5, CSS3, Bootstrap, <br /> React Router, Fontawesome, Netlify, Git, Github</p>
+    //                     </div>
+    //                 </div>
+    //                 {/* A_Doctors Card */}
+
+    //                 {/* MySchool Card */}
+    //                 <div data-aos="fade-left" id="Project2" className="col-md-6 hvr-grow">
+    //                     <img src={School1} class="d-block w-100 Images" alt="..." />
+    //                     <button onMouseEnter={handleProject2} className="Details">Details</button>
+    //                 </div>
+    //                 <div id="Project1_Details2" className="col-md-6 justify-content-center ms-auto text-white">
+    //                     <div className="ms-5 mb-5">
+    //                         <h1>My_School</h1>
+    //                         <p>Full Stack Project</p>
+    //                         <ul className="text-secondary">
+    //                             <li>A School Management System.</li>
+    //                             <li>Teachers information..</li>
+    //                             <li>All notice.</li>
+    //                             <li>Students informations.</li>
+    //                             <li>Admin/Teachers can add or remove a notice.</li>
+    //                             <li>Admin/Teachers are able to update results.</li>
+    //                             <li>Students can update theis informations.</li>
+    //                         </ul>
+    //                         <p className="text-secondary"> <b>Tech and Tools: </b>JavaScript, React, Node, Express, HTML5, CSS3, Bootstrap, <br /> MaterialUI, React Router, Fontawesome, Git, Github</p>
+    //                     </div>
+    //                 </div>
+    //                 {/* MySchool Card */}
+
+    //                 {/* AshopBD Card */}
+    //                 <div data-aos="fade-right" id="Project3" className="col-md-6 hvr-grow mt-4">
+    //                     <img src={Ashop1} class="d-block w-100 Images" alt="..." />
+    //                     <button onMouseEnter={handleProject3} className="Details">Details</button>
+    //                 </div>
+    //                 <div id="Project1_Details3" className="col-md-6 justify-content-center ms-auto text-white">
+    //                     <div className="mb-5 ms-5">
+    //                         <h1> AshopBD</h1>
+    //                         <p>Full Stack Website</p>
+    //                         <ul className="text-secondary">
+    //                             <li>Awesome E-commerce website.</li>
+    //                             <li>Select product, add to cart, remove from cart options</li>
+    //                             <li>Google Authentication and Privet Routes.</li>
+    //                         </ul>
+    //                         <p className="text-secondary"> <b>Tech and Tools: </b> JavaScript, ES6, React, Material UI, NodeJS, <br /> ExpressJS, HTML5, CSS3, Bootstrap5, Firebase Authentication, React Router, Git, Github
+    //                         </p>
+    //                     </div>
+    //                 </div>
+    //                 {/* A_shopBD Card */}
+
+    //                 {/* StoreX Card */}
+    //                 <div data-aos="fade-left" id="Project4" className="col-md-6 hvr-grow mt-4">
+    //                     <img src={StoreX1} class="d-block w-100 Images" alt="..." />
+    //                     <button onMouseEnter={handleProject4} className="Details">Details</button>
+    //                 </div>
+    //                 <div id="Project1_Details4" className="col-md-6 justify-content-center ms-auto text-white">
+    //                     <div className="mb-5 ms-5">
+    //                         <h1> Store_X</h1>
+    //                         <p>PSD to HTML</p>
+    //                         <ul className="text-secondary">
+    //                             <li>HTML5</li>
+    //                             <li>CSS3</li>
+    //                             <li>Bootstrap</li>
+    //                             <li>Font Awesome</li>
+    //                         </ul>
+    //                     </div>
+    //                 </div>
+    //                 {/* StoreX Card */}
+
+    //             </div> */}
+
+
     return (
         <section className="container pb-5 pt-5">
             <h4 data-aos="flip-left" className="text-white mt-5 mb-5 pb-5">Projects ——</h4>
 
-            <div className="row pb-5">
-                {/* A_Doctors Card */}
-                <div data-aos="fade-right" id="Project1" className="col-md-6 hvr-grow">
-                    <img src={Doctor1} class="d-block w-100 Images" alt="..." />
-                    {/* Details Button */}
-                    <button onMouseEnter={handleProject1} className="Details">Details</button>
-                </div>
-
-                <div id="Project1_Details1" className="col-md-6 justify-content-center ms-auto text-white">
-                    <div className="mb-5 ms-5">
-                        <h1>A_Doctors</h1>
-                        <p>Full Stack Project</p>
-                        <ul className="text-secondary">
-                            <li>Dentist Appointment taking website.</li>
-                            <li>Choose the service you want.</li>
-                            <li>Pick any flexible date for appointment.</li>
-                        </ul>
-                        <p className="text-secondary"> <b>Tech and Tools: </b>JavaScript, React, HTML5, CSS3, Bootstrap, <br /> React Router, Fontawesome, Netlify, Git, Github</p>
+            <section>
+                <div className="container">
+                    <div className="row">
+                        {/* School card */}
+                        <div className="col-md-4">
+                            <div className="Card">
+                                {/* Image */}
+                                <img style={{ width: '100%', height: '100%' }} src={School1} alt="" />
+                                {/* Description overlay */}
+                                <div className="container Description">
+                                    <h4 className="mt-4">MySchool</h4>
+                                    <p> Full Stack School Managment Project</p>
+                                    {/* Buttons */}
+                                    <div className="d-flex">
+                                        {/* Live site */}
+                                        <a href="https://nervous-bohr-bb27db.netlify.app/" target="_blank">
+                                            <div>
+                                                <button className="Preview--Button"> <FontAwesomeIcon icon={faEye} /> Preview</button>
+                                            </div>
+                                        </a>
+                                        {/* Details */}
+                                        <div>
+                                            <button className="Details--Button">Details <FontAwesomeIcon className="pt-1" icon={faCaretRight} /></button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        {/* School card */}
+                        {/* A_ShopBD card */}
+                        <div className="col-md-4">
+                            <div className="Card">
+                                {/* Image */}
+                                <img style={{ width: '100%', height: '100%' }} src={Ashop1} alt="" />
+                                {/* Description overlay */}
+                                <div className="container Description">
+                                    <h4 className="mt-4">A_ShopBD</h4>
+                                    <p> Full Stack E-Commerce Project</p>
+                                    {/* Buttons */}
+                                    <div className="d-flex">
+                                        {/* Live site */}
+                                        <a href="https://ema-john-7355e.web.app/" target="_blank">
+                                            <div>
+                                                <button className="Preview--Button"> <FontAwesomeIcon icon={faEye} /> Preview</button>
+                                            </div>
+                                        </a>
+                                        {/* Details */}
+                                        <div>
+                                            <button className="Details--Button">Details <FontAwesomeIcon className="pt-1" icon={faCaretRight} /></button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        {/* A_ShopBD card */}
+                        {/* School card */}
+                        <div className="col-md-4">
+                            <div className="Card">
+                                {/* Image */}
+                                <img style={{ width: '100%', height: '100%' }} src={StoreX1} alt="" />
+                                {/* Description overlay */}
+                                <div className="container Description">
+                                    <h4 className="mt-4">StoreX</h4>
+                                    <p> PSD to HTML</p>
+                                    {/* Buttons */}
+                                    <div className="d-flex">
+                                        {/* Live site */}
+                                        <a href="https://imtious-coder.github.io/Store_X/" target="_blank">
+                                            <div>
+                                                <button className="Preview--Button"> <FontAwesomeIcon icon={faEye} /> Preview</button>
+                                            </div>
+                                        </a>
+                                        {/* Details */}
+                                        <div>
+                                            <button className="Details--Button">Details <FontAwesomeIcon className="pt-1" icon={faCaretRight} /></button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        {/* School card */}
                     </div>
                 </div>
-                {/* A_Doctors Card */}
-
-                {/* MySchool Card */}
-                <div data-aos="fade-left" id="Project2" className="col-md-6 hvr-grow">
-                    <img src={School1} class="d-block w-100 Images" alt="..." />
-                    <button onMouseEnter={handleProject2} className="Details">Details</button>
-                </div>
-                <div id="Project1_Details2" className="col-md-6 justify-content-center ms-auto text-white">
-                    <div className="ms-5 mb-5">
-                        <h1>My_School</h1>
-                        <p>Full Stack Project</p>
-                        <ul className="text-secondary">
-                            <li>A School Management System.</li>
-                            <li>Teachers information..</li>
-                            <li>All notice.</li>
-                            <li>Students informations.</li>
-                            <li>Admin/Teachers can add or remove a notice.</li>
-                            <li>Admin/Teachers are able to update results.</li>
-                            <li>Students can update theis informations.</li>
-                        </ul>
-                        <p className="text-secondary"> <b>Tech and Tools: </b>JavaScript, React, Node, Express, HTML5, CSS3, Bootstrap, <br /> MaterialUI, React Router, Fontawesome, Git, Github</p>
-                    </div>
-                </div>
-                {/* MySchool Card */}
-
-                {/* AshopBD Card */}
-                <div data-aos="fade-right" id="Project3" className="col-md-6 hvr-grow mt-4">
-                    <img src={Ashop1} class="d-block w-100 Images" alt="..." />
-                    <button onMouseEnter={handleProject3} className="Details">Details</button>
-                </div>
-                <div id="Project1_Details3" className="col-md-6 justify-content-center ms-auto text-white">
-                    <div className="mb-5 ms-5">
-                        <h1> AshopBD</h1>
-                        <p>Full Stack Website</p>
-                        <ul className="text-secondary">
-                            <li>Awesome E-commerce website.</li>
-                            <li>Select product, add to cart, remove from cart options</li>
-                            <li>Google Authentication and Privet Routes.</li>
-                        </ul>
-                        <p className="text-secondary"> <b>Tech and Tools: </b> JavaScript, ES6, React, Material UI, NodeJS, <br /> ExpressJS, HTML5, CSS3, Bootstrap5, Firebase Authentication, React Router, Git, Github
-                        </p>
-                    </div>
-                </div>
-                {/* A_shopBD Card */}
-
-                {/* StoreX Card */}
-                <div data-aos="fade-left" id="Project4" className="col-md-6 hvr-grow mt-4">
-                    <img src={StoreX1} class="d-block w-100 Images" alt="..." />
-                    <button onMouseEnter={handleProject4} className="Details">Details</button>
-                </div>
-                <div id="Project1_Details4" className="col-md-6 justify-content-center ms-auto text-white">
-                    <div className="mb-5 ms-5">
-                        <h1> Store_X</h1>
-                        <p>PSD to HTML</p>
-                        <ul className="text-secondary">
-                            <li>HTML5</li>
-                            <li>CSS3</li>
-                            <li>Bootstrap</li>
-                            <li>Font Awesome</li>
-                        </ul>
-                    </div>
-                </div>
-                {/* StoreX Card */}
-
-            </div>
+            </section>
 
             {/* See more button.. */}
-            <div className="container">
+            <div className="container mt-5">
                 <div className="row">
-                    <div className="col-md-2 d-flex justify-content-center">
+                    <div className="col-md-2 ms-auto d-flex justify-content-center">
                         <Link to="/allprojects"><button className="SeeMoreButton">See more</button></Link>
                     </div>
                 </div>
