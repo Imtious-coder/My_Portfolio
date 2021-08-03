c065935b6a70c943476df37555bc23db6a261ac1
unknown <iarafat386@gmail.com>
2021-08-03 11:35:37 +0600

    Navbar Design Updated

--- src/Components/Home/Navbar/Navbar.js
+++ src/Components/Home/Navbar/Navbar.js
@@ -11,49 +11,45 @@ const Navbar = () => {
         AOS.init({ duration: 1000 })
     }, [])
     return (
+
         <nav style={{ backgroundColor: "#303133" }} class="navbar navbar-expand-lg navbar-light ps-5 sticky-top shadow-sm">
             <div class="container-fluid">
                 {/* Toggle Button */}
+                <a class="Name me-5 Font1" href="#">Imtious</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                {/* Main Navigations */}
                 <div class="collapse navbar-collapse" id="navbarNav">
-                    <div className=" container d-flex justify-content-center">
-                        <ul class="navbar-nav ms-auto me-auto">
-                            <Link to="/home">
-                                <li class="nav-item me-5">
-                                    <a class="nav-link active text-white mt-2 hvr-bob Nav" aria-current="page" href="#">Home</a>
-                                </li>
-                            </Link>
-                            <Link to="/resume">
-                                <li class="nav-item me-5">
-                                    <a class="nav-link text-white mt-2 hvr-bob Nav" href="#">Resume</a>
-                                </li>
-                            </Link>
-                            <Link to="/allprojects">
-                                <li class="nav-item me-5">
-                                    <a class="nav-link text-white mt-2 hvr-bob Nav" href="#">Projects</a>
-                                </li>
-                            </Link>
-                            <li class="nav-item me-5">
-                                <a class="nav-link text-white Name" href="#">"AS"</a>
+                    <ul class="navbar-nav ms-auto">
+                        <Link to="/home">
+                            <li class="nav-item me-4 ms-3 float-start">
+                                <a class="nav-link active text-white hvr-bob Nav" aria-current="page" href="#">Home</a>
                             </li>
-                            <Link to="/brain">
-                                <li class="nav-item me-5">
-                                    <a class="nav-link text-white hvr-bob mt-2 Nav" href="#">Brain</a>
-                                </li>
-                            </Link>
-                            <li class="nav-item me-5">
-                                <a class="nav-link text-white hvr-bob mt-2 Nav" href="https://medium.com/@iarafat386" target="_blank">Articles</a>
+                        </Link>
+                        <Link to="/resume">
+                            <li class="nav-item me-4 ms-3 text-center">
+                                <a class="nav-link text-white hvr-bob Nav" href="#">Resume</a>
                             </li>
-                            <Link to="/about">
-                                <li class="nav-item me-5">
-                                    <a class="nav-link text-white hvr-bob mt-2 Nav" href="#">About</a>
-                                </li>
-                            </Link>
-                        </ul>
-                    </div>
+                        </Link>
+                        <Link to="/allprojects">
+                            <li class="nav-item me-4 ms-3 float-end">
+                                <a class="nav-link text-white hvr-bob Nav" href="#">Projects</a>
+                            </li>
+                        </Link>
+                        <Link to="/brain">
+                            <li class="nav-item me-4 ms-3 float-start">
+                                <a class="nav-link text-white hvr-bob Nav" href="#">Brain</a>
+                            </li>
+                        </Link>
+                        <li class="nav-item me-4 ms-3 text-center">
+                            <a class="nav-link text-white hvr-bob Nav" href="https://medium.com/@iarafat386" target="_blank" rel="noreferrer">Articles</a>
+                        </li>
+                        <Link to="/about">
+                            <li class="nav-item me-4 ms-3 float-end">
+                                <a class="nav-link text-white hvr-bob Nav" href="#">About</a>
+                            </li>
+                        </Link>
+                    </ul>
                 </div>
             </div>
         </nav>
