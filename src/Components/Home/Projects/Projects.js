48c7a5cad28061ddc1316860ee92ad98d473f1a1
unknown <iarafat386@gmail.com>
2021-08-05 23:03:48 +0600

    Project Section Updated

--- src/Components/Home/Projects/Projects.js
+++ src/Components/Home/Projects/Projects.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import ProjectData from './ProjectData.json';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEye, faAngleRight } from '@fortawesome/free-solid-svg-icons'
@@ -7,219 +8,59 @@ import { faEye, faAngleRight } from '@fortawesome/free-solid-svg-icons'
 import AOS from 'aos';
 import './Projects.css';
 import 'aos/dist/aos.css';
-
-// images
-import Doctor1 from './A_Doctors.png';
-import StoreX1 from './StoreX1.jpg';
-import Temperature1 from './Temperature1.png';
-import Ashop1 from './Ashop1.png';
-import School1 from './School1.jpg';
-import Amazon1 from './Amazon1.png';
-import Calculator from './calculator.png';
+import { useState } from 'react';
 
 const Projects = () => {
+    const [data, setData] = useState([]);
+
     useEffect(() => {
         AOS.init({ duration: 1000 })
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        setData(ProjectData)
+        console.log(data);
+    }, []);
 
     return (
         <section className="container pb-5 pt-5">
+
             <h4 data-aos="flip-left" className="text-white mt-5 mb-5 pb-5">Projects ——</h4>
             <section>
                 <div className="container">
                     <div className="row">
+                        {
+                            data.map(project =>  {
+                                return (
+                                    <div data-aos="fade-right" className="col-md-4 mb-5">
+                                    <div className="Card">
+                                        {/* Image */}
+                                        <img className="Card_Images" src={project.image} alt="" />
+                                        {/* Description overlay */}
+                                        <div className="container Description">
+                                            <p className="mt-2 Card_Title">{project.name}</p>
+                                            <p className="Card_Description">{project.description}</p>
+                                            <p className="Card_Info">{project.information}</p>
+                                            {/* Buttons */}
+                                            <div className="d-flex">
+                                                {/* Live site */}
+                                                <a href={project.preview} target="_blank" rel="noreferrer">
+                                                    <div>
+                                                        <button className="Preview--Button"> <FontAwesomeIcon icon={faEye} /> Preview</button>
+                                                    </div>
+                                                </a>
+                                                {/* Details */}
+                                                <div>
+                                                    <button className="Details--Button">Details <FontAwesomeIcon icon={faAngleRight} /></button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                )
+                            })
+                        }
 
-                        {/* Calculator card */}
-                        <div data-aos="fade-right" className="col-md-4 mb-5">
-                            <div className="Card">
-                                {/* Image */}
-                                <img className="Card_Images" src={Calculator} alt="" />
-                                {/* Description overlay */}
-                                <div className="container Description">
-                                    <p className="mt-2 Card_Title">Calculator</p>
-                                    <p className="Card_Description"> Front-End Development</p>
-                                    <p className="Card_Info">A Frontend website based in JavaScript(React).</p>
-                                    {/* Buttons */}
-                                    <div className="d-flex">
-                                        {/* Live site */}
-                                        <a href="https://dreamy-curran-97dd26.netlify.app/" target="_blank" rel="noreferrer">
-                                            <div>
-                                                <button className="Preview--Button"> <FontAwesomeIcon icon={faEye} /> Preview</button>
-                                            </div>
-                                        </a>
-                                        {/* Details */}
-                                        <div>
-                                            <button className="Details--Button">Details <FontAwesomeIcon icon={faAngleRight} /></button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        {/* Calculator card */}
-
-                        {/* A_Doctors card */}
-                        <div data-aos="fade-right" className="col-md-4 mb-5">
-                            <div className="Card">
-                                {/* Image */}
-                                <img className="Card_Images" src={Doctor1} alt="" />
-                                {/* Description overlay */}
-                                <div className="container Description">
-                                    <p className="mt-2 Card_Title">A_Doctors</p>
-                                    <p className="Card_Description"> Front-End Development</p>
-                                    <p className="Card_Info">Doctors appointment taking website by JavaScript(React).</p>
-                                    {/* Buttons */}
-                                    <div className="d-flex">
-                                        {/* Live site */}
-                                        <a href="https://keen-sinoussi-d5c500.netlify.app/" target="_blank" rel="noreferrer">
-                                            <div>
-                                                <button className="Preview--Button"> <FontAwesomeIcon icon={faEye} /> Preview</button>
-                                            </div>
-                                        </a>
-                                        {/* Details */}
-                                        <div>
-                                            <button className="Details--Button">Details <FontAwesomeIcon  icon={faAngleRight} /></button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        {/* A_Doctors card */}
-                        {/* StoreX card */}
-                        <div data-aos="flip-right" className="col-md-4 mb-5">
-                            <div className="Card">
-                                {/* Image */}
-                                <img className="Card_Images" src={StoreX1} alt="" />
-                                {/* Description overlay */}
-                                <div className="container Description">
-                                    <p className="mt-2 Card_Title">StoreX</p>
-                                    <p className="Card_Description"> PSD to HTML</p>
-                                    <p className="Card_Info">A Pixel Perfect fully responsive PSD to HTML site. </p>
-                                    {/* Buttons */}
-                                    <div className="d-flex">
-                                        {/* Live site */}
-                                        <a href="https://imtious-coder.github.io/Store_X/" target="_blank" rel="noreferrer">
-                                            <div>
-                                                <button className="Preview--Button"> <FontAwesomeIcon icon={faEye} /> Preview</button>
-                                            </div>
-                                        </a>
-                                        {/* Details */}
-                                        <div>
-                                            <button className="Details--Button">Details <FontAwesomeIcon  icon={faAngleRight} /></button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        {/* StoreX card */}
-                        {/* Temperature-Finder card */}
-                        <div data-aos="fade-left" className="col-md-4 mb-5">
-                            <div className="Card">
-                                {/* Image */}
-                                <img className="Card_Images" src={Temperature1} alt="" />
-                                {/* Description overlay */}
-                                <div className="container Description">
-                                    <p className="mt-2 Card_Title">Temperature-Finder</p>
-                                    <p className="Card_Description"> Most updated temperature</p>
-                                    <p className="Card_Info">Frontend based temperature finder website by React.js</p>
-                                    {/* Buttons */}
-                                    <div className="d-flex">
-                                        {/* Live site */}
-                                        <a href="https://imtious-coder.github.io/Temperature-Finder/" target="_blank" rel="noreferrer">
-                                            <div>
-                                                <button className="Preview--Button"> <FontAwesomeIcon icon={faEye} /> Preview</button>
-                                            </div>
-                                        </a>
-                                        {/* Details */}
-                                        <div>
-                                            <button className="Details--Button">Details <FontAwesomeIcon  icon={faAngleRight} /></button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        {/* Temperature-Finder card */}
-                        {/* My_School card */}
-                        <div data-aos="fade-right" className="col-md-4 mb-5">
-                            <div className="Card">
-                                {/* Image */}
-                                <img className="Card_Images" src={School1} alt="" />
-                                {/* Description overlay */}
-                                <div className="container Description">
-                                    <p className="mt-2 Card_Title">My_School</p>
-                                    <p className="Card_Description"> School management system</p>
-                                    <p className="Card_Info">Ongoing full-stack project including all management system for a school.</p>
-                                    {/* Buttons */}
-                                    <div className="d-flex">
-                                        {/* Live site */}
-                                        <a href="https://myschool-com.web.app/" target="_blank" rel="noreferrer">
-                                            <div>
-                                                <button className="Preview--Button"> <FontAwesomeIcon icon={faEye} /> Preview</button>
-                                            </div>
-                                        </a>
-                                        {/* Details */}
-                                        <div>
-                                            <button className="Details--Button">Details <FontAwesomeIcon  icon={faAngleRight} /></button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        {/* My_School card */}
-                        {/* A_ShopBD card */}
-                        <div data-aos="flip-right" className="col-md-4 mb-5">
-                            <div className="Card">
-                                {/* Image */}
-                                <img className="Card_Images" src={Ashop1} alt="" />
-                                {/* Description overlay */}
-                                <div className="container Description">
-                                    <p className="mt-2 Card_Title">A_ShopBD</p>
-                                    <p className="Card_Description"> E-commerce Web App</p>
-                                    <p className="Card_Info">Full-stack e-commerce project by React, Node, and MongoDB.</p>
-                                    {/* Buttons */}
-                                    <div className="d-flex">
-                                        {/* Live site */}
-                                        <a href="https://ema-john-7355e.web.app/" target="_blank" rel="noreferrer">
-                                            <div>
-                                                <button className="Preview--Button"> <FontAwesomeIcon icon={faEye} /> Preview</button>
-                                            </div>
-                                        </a>
-                                        {/* Details */}
-                                        <div>
-                                            <button className="Details--Button">Details <FontAwesomeIcon  icon={faAngleRight} /></button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        {/* A_ShopBD card */}
-                        {/* Amazon_Login_CLone card */}
-                        <div data-aos="fade-left" className="col-md-4 mb-5">
-                            <div className="Card">
-                                {/* Image */}
-                                <img className="Card_Images" src={Amazon1} alt="" />
-                                {/* Description overlay */}
-                                <div className="container Description">
-                                    <p className="mt-2 Card_Title">Amazon_Login_CLone</p>
-                                    <p  className="Card_Description"> Login system</p>
-                                    <p className="Card_Info">Frontend based fully cloned login system of amazon.</p>
-                                    {/* Buttons */}
-                                    <div className="d-flex">
-                                        {/* Live site */}
-                                        <a href="https://fir-14da6.web.app/" target="_blank" rel="noreferrer">
-                                            <div>
-                                                <button className="Preview--Button"> <FontAwesomeIcon icon={faEye} /> Preview</button>
-                                            </div>
-                                        </a>
-                                        {/* Details */}
-                                        <div>
-                                            <button className="Details--Button">Details <FontAwesomeIcon  icon={faAngleRight} /></button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        {/* Amazon_Login_CLone card */}
-                        
                     </div>
                 </div>
             </section>
