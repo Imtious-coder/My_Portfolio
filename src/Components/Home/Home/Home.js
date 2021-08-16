4c39f4aa5efbad2894091904b3d0d8d7646716ce
unknown <iarafat386@gmail.com>
2021-08-16 11:45:23 +0600

    Home Page updated

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Articles from '../Articles/Articles';
 import Description from '../Description/Description';
-import Footer from '../Footer/Footer';
 import Form from '../Form/Form';
 import Projects from '../Projects/Projects';
 
@@ -9,7 +8,7 @@ const Home = () => {
     return (
         <div style={{ backgroundColor: '#303133' }}>
             <Description></Description>
-            <div style={{ borderBottom: '1px solid grey', width: '75%' }} className="container mt-5 pt-5"></div>
+            <div style={{ borderBottom: '1px solid grey', width: '86%' }} className="container mt-5 pt-5"></div>
             <Projects></Projects>
             <div style={{ borderBottom: '1px solid grey', width: '75%' }} className="container mt-5 pt-5"></div>
             <Articles></Articles>
