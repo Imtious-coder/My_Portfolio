c1c5fc89cd08dfcba77169d86c414f9d1411c740
Imtious-coder <iarafat386@gmail.com>
2021-09-17 20:00:06 +0600

    Description Updated

--- src/Components/Home/Description/Description.js
+++ src/Components/Home/Description/Description.js
@@ -21,9 +21,12 @@ const Description = () => {
                     {/* Name */}
                     <h1 className="text-white pt-5 mt-5 mb-1">Imtious Islam Midul</h1>
                     {/* Title */}
-                    <h5 className="text-white mb-3">Front-End Developer</h5>
+                    <h5 className="text-white mb-3"> JavaScript Developer (M E R N - S T A C K)</h5>
                     {/* Description */}
-                    <small className="text-secondary Description_Details">A passionate and curious Frontend Developer. Ready to start a challenging role at a progressive organization to implement my skills and knowledge with the goal of the organization’s success. Excellent time management skills with experience of 20+ projects, including e-commerce applications. I’ve got a good grasp of the latest technologies and tools.
+                    <small className="text-secondary Description_Details">2+ years of professional experience in Front-End Development.
+                        Looking for a challenging role at a progressive organization to grow
+                        my skills and knowledge with the goal of the organization’s success.
+                        Highly passionate and skilled in front-end development with pixelperfect design and the use of the latest technologies and tools.
                     </small> <br />
                     {/* Resume Download Button */}
                     <a href={Resume} target="_blank" className="hvr-grow" rel="noreferrer" download><button className="Button mt-3">Resume<FontAwesomeIcon className="Button-Icon ms-1" icon={faArrowAltCircleDown} /></button></a>
