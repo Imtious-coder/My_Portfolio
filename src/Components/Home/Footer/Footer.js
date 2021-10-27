8a6d66f8b46b3d98b27ef11c096e9c2c0bd06754
Imtious-coder <iarafat386@gmail.com>
2021-10-27 07:54:02 +0600

    UI updated, Project Added

--- src/Components/Home/Footer/Footer.js
+++ src/Components/Home/Footer/Footer.js
@@ -16,16 +16,40 @@ const Footer = () => {
                 <div className="row">
                     {/* Socila links */}
                     <div className="col-md-4">
-                        <p id="Social">Say Hello . . .</p>
+                        <p id="Social">
+                            Say Hello . . .
+                        </p>
                         {/* Icons */}
-                        <a href="https://www.facebook.com/profile.php?id=100030607042215" ><FontAwesomeIcon id="Social-Icon1" icon={faFacebook} /></a>
-                        <a href="https://github.com/Imtious-coder" ><FontAwesomeIcon id="Social-Icon2" icon={faGitAlt} /></a>
-                        <a href="https://www.linkedin.com/in/imtious-islam-842a73189/" ><FontAwesomeIcon id="Social-Icon3" icon={faLinkedin} /></a>
+                        <a
+                            href="https://www.facebook.com/profile.php?id=100030607042215" >
+                            <FontAwesomeIcon
+                                id="Social-Icon1"
+                                icon={faFacebook} />
+                        </a>
+                        <a
+                            href="https://github.com/Imtious-coder" >
+                            <FontAwesomeIcon
+                                id="Social-Icon2"
+                                icon={faGitAlt} />
+                        </a>
+                        <a
+                            href="https://www.linkedin.com/in/imtious-islam-842a73189/" >
+                            <FontAwesomeIcon
+                                id="Social-Icon3"
+                                icon={faLinkedin} />
+                        </a>
                     </div>
                     <div className="col-md-4"></div>
                     {/* Copyright */}
-                    <div className="col-md-4 d-flex align-items-center text-white">
-                        <p>© All rights 2021 reserved by "@Imtious"</p>
+                    <div
+                        className="
+                            col-md-4 
+                            d-flex 
+                            align-items-center 
+                            text-white">
+                        <p>
+                            © All rights 2021 reserved by "@Imtious"
+                        </p>
                     </div>
                 </div>
             </div>
